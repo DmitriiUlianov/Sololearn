39c80ed4eea1adf9461a26c8bfe8bacec543b7d6
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-11-08 22:29:45 +0100

    Create and_or.py

---
+++ Codes_Python/and_or.py
@@ -0,0 +1,7 @@
+x = [0, 0, 0]
+y = [1, 1, 1]
+print(x and y) # "and" operator returns the first falsy operand if one exists; otherwise, it returns the last operand.
+print(x or y)  # "or" operator returns the first truthy operand if it finds one; otherwise, it returns the last operand.
+
+[1, 1, 1]
+[0, 0, 0]
