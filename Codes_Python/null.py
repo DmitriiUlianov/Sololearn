eb0bab710d6ec40ce807a41c6fa78c9e26ac70fd
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-11-08 22:42:57 +0100

    Create null.py

---
+++ Codes_Python/null.py
@@ -0,0 +1,10 @@
+# the 'null' object is the object None
+
+'''
+It is used to represent the absence of a value or a null value.
+
+Usage: None is commonly used:
+- As a default return value for functions that don’t explicitly return a value.
+- To indicate missing or undefined data.
+- To signal the absence of a specific value (such as default arguments in function definitions).
+'''
