6094a0756c2fbd2ffd8ff90446b2bc668798f78e
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-11-09 11:39:56 +0100

    Create index.py

---
+++ Codes_Python/index.py
@@ -0,0 +1,12 @@
+x = 'abcda'
+for i in x:
+    if x.index(i) == 0:
+        print(i)
+
+a
+a
+
+'''
+The index() method returns the position at the first occurrence of the specified value.
+'a' appears twice in x, and each time i is 'a', x.index(i) is 0 because the first occurrence of 'a' is at index 0.
+'''
