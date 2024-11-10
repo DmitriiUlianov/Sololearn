39c739c458409fb3a46e3b616e416f93a2aa5a24
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-11-10 12:13:59 +0100

    Create reverse_slice.py

---
+++ Codes_Python/reverse_slice.py
@@ -0,0 +1,7 @@
+print('game of dice'[::-1]) # Output after reverse: ecid fo emag
+
+print('game of dice'[::-3]) # Output after reverse and slicing: edom
+
+'''
+[::-3], it reverses the string and selects every third character starting from the end.
+'''
