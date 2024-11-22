0aea39a37e66ab4634e216258b800fb3cc013c07
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-11-22 23:15:00 +0100

    Create function 2.py

---
+++ Codes_Python/function 2.py
@@ -0,0 +1,14 @@
+def f(n=9):
+    print(n) # 5
+f(5)
+
+'''
+1. Function Definition:
+def f(n=9) defines a function f with one parameter n. The default value of n is set to 9, meaning if the function is called without an argument, n will default to 9.
+
+2. Calling the Function:
+f(5) calls the function f, passing the value 5 as the argument for n. This overrides the default value of n (which is 9).
+
+3. Inside the Function:
+Inside the function, print(n) prints the value of n, which is 5 in this case, because 5 was passed as the argument.
+'''
